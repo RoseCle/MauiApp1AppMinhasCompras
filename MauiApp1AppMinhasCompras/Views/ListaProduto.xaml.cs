0cfb1e5736ff65d4d65849f03b2c04edbfdbfcd4
RoseCle <rosecle.educacional@gmail.com>
2025-03-24 20:24:44 -0300

    Aprendendo a usar a SearchBar

--- MauiApp1AppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiApp1AppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,25 @@
+using MauiApp1AppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiApp1AppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+	
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach(i  => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -17,5 +31,30 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O Total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "Ok");
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
